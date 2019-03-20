53bee098eb00966905b915d0634e7d73c8ca1289
Vincent Yao <qiuchen_yao@126.com>
2019-03-20 23:09:21 +0800

    M11

---
+++ source/src/main/java/array/M11.java
@@ -0,0 +1,27 @@
+package array;
+
+/**
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
+public class M11 {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
