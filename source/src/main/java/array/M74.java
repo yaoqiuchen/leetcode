de22611cc7c4714bde99c116650c26c57eebf4f2
Vincent Yao <qiuchen_yao@126.com>
2019-03-27 16:26:49 +0800

    M74

---
+++ source/src/main/java/array/M74.java
@@ -0,0 +1,63 @@
+package array;
+
+/**
+ * 搜索二维矩阵
+ *
+ 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+ 每行中的整数从左到右按升序排列。
+ 每行的第一个整数大于前一行的最后一个整数。
+ 示例 1:
+
+ 输入:
+ matrix = [
+ [1,   3,  5,  7],
+ [10, 11, 16, 20],
+ [23, 30, 34, 50]
+ ]
+ target = 3
+ 输出: true
+ 示例 2:
+
+ 输入:
+ matrix = [
+ [1,   3,  5,  7],
+ [10, 11, 16, 20],
+ [23, 30, 34, 50]
+ ]
+ target = 13
+ 输出: false
+
+ */
+public class M74 {
+
+    public static void main(String[] args) {
+        new M74().searchMatrix(new int[][] {{0,1,1},{1,1,1},{1,1,1} }, 5);
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int val = getVal(matrix, mid);
+            if (target < val) {
+                high = mid - 1;
+            } else if (target > val) {
+                low = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getVal(int[][] matrix, int idx) {
+        int x = idx / matrix[0].length;
+        int y = idx % matrix[0].length;
+        return matrix[x][y];
+    }
+}
