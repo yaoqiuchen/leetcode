501e9c2a7bfb29d59bb8b96d7969ba9bdf50a674
Vincent Yao <qiuchen_yao@126.com>
2019-04-20 09:06:49 +0800

    M217

---
+++ source/src/main/java/array/E217.java
@@ -0,0 +1,41 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 217. 存在重复元素
+ *
+ 给定一个整数数组，判断是否存在重复元素。
+
+ 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+
+ 示例 1:
+
+ 输入: [1,2,3,1]
+ 输出: true
+ 示例 2:
+
+ 输入: [1,2,3,4]
+ 输出: false
+ 示例 3:
+
+ 输入: [1,1,1,3,3,4,3,2,4,2]
+ 输出: true
+ */
+public class E217 {
+
+    public static void main(String[] args) {
+        new E217().containsDuplicate(new int[] {});
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+
+}
