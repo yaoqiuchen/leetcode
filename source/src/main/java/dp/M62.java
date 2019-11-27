4b99d7582f6fa6e501be2c8b8bc2f8895a0faad9
Vincent Yao <qiuchen_yao@126.com>
2019-11-27 13:38:47 +0800

    M62

--- source/src/main/java/dp/M62.java
+++ source/src/main/java/dp/M62.java
@@ -20,4 +20,15 @@ public class M62 {
         }
         return dp[m-1][n-1];
     }
+
+    // 2019-11-27
+    public int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j< n; j++) {
+                dp[i][j] = (i == 0 || j == 0) ? 1 : dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 }
