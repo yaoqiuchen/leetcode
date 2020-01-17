6afef16226d895dfbc91fb29c1d3e31881e88237
Vincent Yao <qiuchen_yao@126.com>
2020-01-17 22:06:41 +0800

    M15 三数之和

--- source/src/main/java/array/M15.java
+++ source/src/main/java/array/M15.java
@@ -21,6 +21,26 @@ import java.util.*;
  [-1, -1, 2]
  ]
 
+
+ 15. 三数之和
+
+ 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+ 找出所有满足条件且不重复的三元组。
+
+ 注意：答案中不可以包含重复的三元组。
+
+  
+
+ 示例：
+
+ 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+ 满足要求的三元组集合为：
+ [
+ [-1, 0, 1],
+ [-1, -1, 2]
+ ]
+
  */
 public class M15 {
 
@@ -29,6 +49,44 @@ public class M15 {
 //    }
 
     public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) return new ArrayList<>();
+
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        Integer head = null;
+        for (int i = 0; i < nums.length; i++) {
+            // 第一个节点数字不能重复
+            if (head != null && nums[i] == head) {
+                continue;
+            }
+
+            int first = nums[i], target = 0 - first;
+            head = nums[i];
+
+            int l = i+1, h = nums.length-1;
+            Integer tail = null;
+            while (l < h) {
+                if (tail != null && tail == nums[h]) {
+                    h--;
+                    continue;
+                }
+                int sum = nums[l] + nums[h];
+                if (nums[l] + nums[h] == target) {
+                    res.add(Arrays.asList(first, nums[l], nums[h]));
+                    tail = nums[h];
+                    h--;
+                    l++;
+                    continue;
+                }
+
+                if (sum > target) h--;
+                else l++;
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> threeSum2(int[] nums) {
         int last = nums.length - 1, len = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
