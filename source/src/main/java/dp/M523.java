0ae278590e83b7199872c592e775607cb452591e
Vincent Yao <qiuchen_yao@126.com>
2020-01-17 21:07:02 +0800

    M523  连续的子数组和

--- source/src/main/java/dp/M523.java
+++ source/src/main/java/dp/M523.java
@@ -4,20 +4,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous
- subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+ 523. 连续的子数组和
 
- Example 1:
- Input: [23, 2, 4, 6, 7],  k=6
- Output: True
- Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+ 给定一个包含非负数的数组和一个目标整数 k，编写一个函数来判断该数组是否含有连续的子数组，
+ 其大小至少为 2，总和为 k 的倍数，即总和为 n*k，其中 n 也是一个整数。
 
- Example 2:
- Input: [23, 2, 6, 4, 7],  k=6
- Output: True
- Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ 示例 1:
+
+ 输入: [23,2,4,6,7], k = 6
+ 输出: True
+ 解释: [2,4] 是一个大小为 2 的子数组，并且和为 6。
+ 示例 2:
+
+ 输入: [23,2,6,4,7], k = 6
+ 输出: True
+ 解释: [23,2,6,4,7]是大小为 5 的子数组，并且和为 42。
+ 说明:
+
+ 数组的长度不会超过10,000。
+ 你可以认为所有数字总和在 32 位有符号整数范围内。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/continuous-subarray-sum
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class M523 {
+
+    public static void main(String[] args) {
+        checkSubarraySum2(new int[] {1,0,1,0,1}, 4);
+    }
+
+    // 2020-1-17
+    public static boolean checkSubarraySum2(int[] nums, int k) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 最近的两个数为0，那任意k都可以满足条件
+            if (nums[i] == 0 && i > 0 && nums[i-1] == 0) {
+                return true;
+            }
+
+            if (k != 0) {
+                // 累加总数，如果本次gap为5，那么只要寻找上一个gap同样为5的数字
+                // 再看两个数字之间的差距是不是大于等于2，就知道这两个数之间的数字之和已经被整除了
+                sum += nums[i];
+                int gap = sum % k;
+
+                if (gap == 0 && i > 0) {
+                    return true;
+                }
+
+                if (dp.containsKey(gap)) {
+                    if (i - dp.get(gap) >= 2) {
+                        return true;
+                    }
+                } else {
+                    dp.put(gap, i);
+                }
+            }
+        }
+
+        return false;
+    }
+
 
     /**
      * Pay attention to some edge cases like k=0
