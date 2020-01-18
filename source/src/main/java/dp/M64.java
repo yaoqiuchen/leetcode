e70e0d0a2d79b832757eac9c3d865ecf9cc409b6
Vincent Yao <qiuchen_yao@126.com>
2020-01-18 21:51:47 +0800

    M64 - 最短路径和 -动态规划

--- source/src/main/java/dp/M64.java
+++ source/src/main/java/dp/M64.java
@@ -18,8 +18,33 @@ package dp;
  */
 public class M64 {
 
+    public static void main(String[] atgs) {
+        minPathSum(new int[][] {{1,3,1}, {1,5,1}, {4,2,1}});
+    }
 
-    public int minPathSum(int[][] grid) {
+    public static  int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[0][0];
+                    continue;
+                }
+
+                if (i == 0) dp[i][j] = dp[i][j-1];
+                else if (j == 0) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]);
+
+                dp[i][j] += grid[i][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    public int minPathSum_(int[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) return 0;
 
         int m = grid.length, n = grid[0].length;
