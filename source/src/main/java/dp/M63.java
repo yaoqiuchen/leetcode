30565d88c7a5523e6ddd1870be1bb0a36fb261c8
Vincent Yao <qiuchen_yao@126.com>
2020-01-18 21:38:14 +0800

    M63 - 不同路径II

--- source/src/main/java/dp/M63.java
+++ source/src/main/java/dp/M63.java
@@ -11,7 +11,31 @@ package dp;
  */
 public class M63 {
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if (i == 0) {
+                    dp[i][j] = (j == 0) ? 1 : dp[i][j-1];
+                } else if (j == 0) {
+                    dp[i][j] = (i == 0) ? 1 : dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    public int uniquePathsWithObstacles_(int[][] obstacleGrid) {
         if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) return 0;
 
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
