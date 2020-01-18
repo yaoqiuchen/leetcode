56ade8514209cd0749889ce12993c314139c5666
Vincent Yao <qiuchen_yao@126.com>
2020-01-18 21:18:37 +0800

    M59 螺旋矩阵 II - 方向

--- source/src/main/java/array/M59.java
+++ source/src/main/java/array/M59.java
@@ -22,7 +22,47 @@ public class M59 {
         new M59().generateMatrix(4);
     }
 
+    // 2020-1-18
     public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+
+        int direction = 1;
+        int[] pos = new int[] {0, 0};
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 1; i <= n*n; i++) {
+            // dp标记已经访问过
+            res[pos[0]][pos[1]] = i;
+            dp[pos[0]][pos[1]] = true;
+
+            // 计算下一个地址，如果已经访问过，或者越界，那么重新计算
+            int[] nextPos = nextPosition(pos, direction);
+            if (shouldRedirect(nextPos, dp, n)) {
+                direction = (direction == 4) ? 1 : direction+1;
+                nextPos = nextPosition(pos, direction);
+            }
+            pos = nextPos;
+        }
+
+        return res;
+    }
+
+    public boolean shouldRedirect(int[] nextPos, boolean[][] dp, int n) {
+        return nextPos[0] < 0 || nextPos[0] >= n ||
+                nextPos[1] < 0 || nextPos[1] >= n ||
+                dp[nextPos[0]][nextPos[1]];
+    }
+
+    public int[] nextPosition(int[] pos, int direction) {
+        switch (direction) {
+            case 1: return new int[] {pos[0], pos[1]+1};
+            case 2: return new int[] {pos[0]+1, pos[1]};
+            case 3: return new int[] {pos[0], pos[1]-1};
+            case 4: return new int[] {pos[0]-1, pos[1]};
+        }
+        return new int[] {};
+    }
+
+    public int[][] generateMatrix2(int n) {
         int res[][] = new int[n][n];
 
         int x = 0, y = 0; // 起始坐标
