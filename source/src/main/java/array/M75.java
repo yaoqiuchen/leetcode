30b1487d8d8194d3eddf8881e7a69aba4d965f8c
Vincent Yao <qiuchen_yao@126.com>
2020-01-19 11:07:01 +0800

    M75 颜色分类 - 只扫描一趟，用多个标记位记录下标

--- source/src/main/java/array/M75.java
+++ source/src/main/java/array/M75.java
@@ -3,7 +3,8 @@ package array;
 /**
  * 颜色分类
  *
- 定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ 定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，
+ 并按照红色、白色、蓝色顺序排列。
 
  此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
 
@@ -29,6 +30,45 @@ public class M75 {
     }
 
     public void sortColors(int[] nums) {
+        int len0 = 0, len1 = 0, len2 = 0;
+        boolean n1 = false, n2 = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                n2 = true;
+                nums[len2] = 2;
+                len2++;
+            }
+            else  {
+                if (nums[i] == 0) {
+                    nums[len0] = 0;
+                    len0++;
+                    if (n1) {
+                        nums[len1] = 1;
+                    }
+                    if (n2) {
+                        nums[len2] = 2;
+                    }
+                    len2++;
+                    len1++;
+                }
+                else if (nums[i] == 1) {
+                    n1 = true;
+                    nums[len1] = 1;
+                    len1++;
+                    if (n2) {
+                        nums[len2] = 2;
+                    }
+                    len2++;
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public void sortColors_(int[] nums) {
         int start = sortColorFromIdx(nums, 0, 0);
         sortColorFromIdx(nums, start, 1);
     }
