5079cd36d32fb96d33407014e89eaa292d0f1705
Vincent Yao <qiuchen_yao@126.com>
2020-01-19 10:23:44 +0800

    M73 - 矩阵置零 - 没有额外空间

--- source/src/main/java/array/M73.java
+++ source/src/main/java/array/M73.java
@@ -48,8 +48,55 @@ public class M73 {
         new M73().setZeroes(new int[][] {{0,1,1},{1,1,1},{1,1,1} });
     }
 
-    // no additional spaces
     public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        if (m == 0 || n == 0) return;
+
+        boolean firstLineZero = false;
+        boolean firstRowZero = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0 && j == 0) {
+                        firstLineZero = true;
+                        firstRowZero = true;
+                        continue;
+                    }
+                    if (i == 0) firstLineZero = true;
+                    if (j == 0) firstRowZero = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 重置第一行
+        if (firstLineZero) {
+            Arrays.fill(matrix[0], 0);
+        }
+        if (firstRowZero) {
+            for (int i = 0; i < ; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+
+
+
+
+
+
+    // no additional spaces
+    public void setZeroes_(int[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return;
         int m = matrix.length, n = matrix[0].length;
 
