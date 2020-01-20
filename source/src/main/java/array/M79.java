412741f68e02d6849fea74feb44af4b913820225
Vincent Yao <qiuchen_yao@126.com>
2020-01-20 16:50:44 +0800

    M79 单词搜索 - DFS+标记

--- source/src/main/java/array/M79.java
+++ source/src/main/java/array/M79.java
@@ -11,7 +11,8 @@ import java.util.List;
 
  给定一个二维网格和一个单词，找出该单词是否存在于网格中。
 
- 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格
+ 。同一个单元格内的字母不允许被重复使用。
 
  示例:
 
@@ -31,10 +32,49 @@ public class M79 {
 
     public static void main(String[] args) {
         char input[][] = new char[][] {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-        new M79().exist(input, "ABCB");
+        new M79().exist(input, "ABKB");
     }
 
+    // 2020-1-20
     public boolean exist(char[][] board, String word) {
+        if (board.length == 0) return false;
+
+        int m = board.length, n = board[0].length;
+        if (m * n < word.length()) return false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean[][] dp = new boolean[m][n];
+                if (exist(board, i, j, word, 0, dp)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean exist(char[][] board, int x, int y, String word, int start, boolean[][] dp) {
+        if (start == word.length()) return true;
+
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length || dp[x][y]) {
+            return false;
+        }
+        if (board[x][y] == word.charAt(start)) {
+            dp[x][y] = true;
+            if (exist(board, x-1, y, word, start+1, dp) ||
+                    exist(board, x, y+1, word, start+1, dp) ||
+                    exist(board, x+1, y, word, start+1, dp) ||
+                    exist(board, x, y-1, word, start+1, dp)) {
+                return true;
+            }
+            dp[x][y] = false;
+        }
+        return false;
+    }
+
+
+
+    public boolean exist_(char[][] board, String word) {
         if (board.length * board[0].length < word.length()) return false;
 
         boolean dp[][] = new boolean[board.length][board[0].length];
