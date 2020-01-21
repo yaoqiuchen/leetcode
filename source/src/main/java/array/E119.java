bd951625a410c7e366ac5e99378a899199c82d04
Vincent Yao <qiuchen_yao@126.com>
2020-01-21 22:37:02 +0800

    E119 - 杨辉三角 II

--- source/src/main/java/array/E119.java
+++ source/src/main/java/array/E119.java
@@ -12,7 +12,27 @@ public class E119 {
         new E119().getRow(3);
     }
 
+
+
     public List<Integer> getRow(int rowIndex) {
+        List<Integer> lastLine = new ArrayList<>();
+        for (int i = 1; i <= rowIndex+1; i++) {
+            List<Integer> newLine = new ArrayList<>(i);
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i-1) {
+                    newLine.add(1);
+                    continue;
+                }
+                int val = lastLine.get(j-1) + lastLine.get(j);
+                newLine.add(val);
+            }
+            lastLine = newLine;
+        }
+        return lastLine;
+    }
+
+
+    public List<Integer> getRow_(int rowIndex) {
         List<Integer> prev = new ArrayList<>();
         List<Integer> cur = new ArrayList<>();
 
