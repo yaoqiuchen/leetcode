87d27ac2e71a321c265dbf907e55db0bc9f790e0
Vincent Yao <qiuchen_yao@126.com>
2020-01-21 20:24:24 +0800

    M105 - 105. 从前序与中序遍历序列构造二叉树 - DFS构造左右子树

--- source/src/main/java/array/M105.java
+++ source/src/main/java/array/M105.java
@@ -30,6 +30,7 @@ public class M105 {
     public static void main(String[] args) {
         new M105().buildTree(new int[] {1,3,4},new int[] {1,3,4});
     }
+
     public class TreeNode {
         int val;
         TreeNode left;
@@ -38,6 +39,42 @@ public class M105 {
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) return null;
+
+        return DFS(preorder, inorder, 0, 0, inorder.length);
+    }
+
+    /**
+     *
+     * @param preorder
+     * @param inorder
+     * @param preorderIdx 前序第一个数字
+     * @param len 子树长度
+     * @return
+     */
+    public TreeNode DFS(int[] preorder, int[] inorder, int preorderIdx, int inorderIdx, int len) {
+        if (len == 0) return null;
+
+        TreeNode node = new TreeNode(preorder[preorderIdx]);
+
+        // 找到前序和中序第一个相同的数字，这之间的距离就是左子树的距离
+        int count = 0;
+        int i = inorderIdx;
+        while (preorder[preorderIdx] != inorder[i]) {
+            i++;
+            count++;
+        }
+
+        // 计算左子树，前序第一个字母跳过，中序不变，树的长度就是count计数
+        node.left = DFS(preorder, inorder, preorderIdx+1, inorderIdx, count);
+        // 计算右子树，前序和中序-跳过第一个字母和左子树长度
+        // 长度，子树当前长度-左子树长度-head
+        node.right = DFS(preorder, inorder, preorderIdx+count+1, inorderIdx+count+1, len-count-1);
+        return node;
+    }
+
+
+    public TreeNode buildTree_(int[] preorder, int[] inorder) {
         return DFS(preorder, 0, preorder.length, inorder, 0);
     }
 
