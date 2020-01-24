fb8fa22f144137b3d33304337061659fb8614a4d
Vincent Yao <qiuchen_yao@126.com>
2020-01-25 00:10:38 +0800

    M216 - 组合总和 III - DFS

--- source/src/main/java/array/M216.java
+++ source/src/main/java/array/M216.java
@@ -25,9 +25,50 @@ import java.util.List;
 public class M216 {
 
     public static void main(String[] args) {
-        new M216().combinationSum3(3, 7);
+        new M216().DFS(3, 7);
     }
-    public List<List<Integer>> combinationSum3(int k, int n) {
+
+
+    public List<List<Integer>> DFS(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n <= 0) return res;
+
+        LinkedList<Integer> tmp = new LinkedList();
+        return DFS(k, n, 1, tmp);
+    }
+
+
+    public List<List<Integer>> DFS(int k, int n, int start, LinkedList<Integer> tmp) {
+        List<List<Integer>> _tmp = new ArrayList<>();
+        if (n == 0 && k == 0) {
+            _tmp.add(new ArrayList(tmp));
+            return _tmp;
+        }
+        // 如果数字大于9了说明前面9个数字都用完了
+        // 如果n<0说明超了
+        // 如果n<start，说明后面的数字都会超
+        if (k > 9 || n <= 0 || n < start) {
+            return null;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            tmp.addLast(i);
+            List<List<Integer>> conb = DFS(k-1, n-i, i+1, tmp);
+            if (conb != null) {
+                _tmp.addAll(conb);
+            }
+            tmp.removeLast();
+        }
+        return _tmp;
+    }
+
+
+
+
+
+
+
+    public List<List<Integer>> combinationSum3_(int k, int n) {
         if (k <= 0 || n <= 0) return new ArrayList<>();
 
         List<List<Integer>> res = new ArrayList<>();
