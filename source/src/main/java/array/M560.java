a0c1662550a2445a76ff332c960cf0a552a8e913
Vincent Yao <qiuchen_yao@126.com>
2020-01-26 00:38:32 +0800

    M560. 和为K的子数组 - 利用Map有O(n)解法

--- source/src/main/java/array/M560.java
+++ source/src/main/java/array/M560.java
@@ -24,8 +24,30 @@ public class M560 {
         new M560().subarraySum(new int[] {1,2,3}, 3);
     }
 
-    // O(n)
+
+    // 2020-1-25
     public int subarraySum(int[] nums, int k) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int res = 0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int gap = sum - k;
+
+            res += map.getOrDefault(gap, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return res;
+    }
+
+
+    // O(n)
+    public int subarraySum_(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
