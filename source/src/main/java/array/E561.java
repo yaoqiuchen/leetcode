65efe586d1904624a108c50d798c1b20b712a3b0
Vincent Yao <qiuchen_yao@126.com>
2020-01-26 00:42:00 +0800

    E561 - 数组拆分 I

--- source/src/main/java/array/E561.java
+++ source/src/main/java/array/E561.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 /**
  561. 数组拆分 I
 
- 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，
+ 使得从1 到 n 的 min(ai, bi) 总和最大。
 
  示例 1:
 
@@ -25,7 +26,20 @@ public class E561 {
     }
 
 
+    // 2020-1-20
     public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0; i < nums.length; i +=2) {
+            res += nums[i];
+        }
+        return res;
+    }
+
+
+
+
+    public int arrayPairSum_(int[] nums) {
         Arrays.sort(nums);
         int sum = 0;
         for (int i = 0; i < nums.length; i+=2) {
