543a78546970802898b2357e27c3cb4553ff5fcf
Vincent Yao <qiuchen_yao@126.com>
2020-01-27 01:34:41 +0800

    M667 - 优美的排列 II

--- source/src/main/java/array/M667.java
+++ source/src/main/java/array/M667.java
@@ -5,7 +5,8 @@ package array;
 
  给定两个整数 n 和 k，你需要实现一个数组，这个数组包含从 1 到 n 的 n 个不同整数，同时满足以下条件：
 
- ① 如果这个数组是 [a1, a2, a3, ... , an] ，那么数组 [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] 中应该有且仅有 k 个不同整数；.
+ ① 如果这个数组是 [a1, a2, a3, ... , an] ，那么数组 [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|]
+ 中应该有且仅有 k 个不同整数；.
 
  ② 如果存在多种答案，你只需实现并返回其中任意一种.
 
@@ -33,10 +34,33 @@ public class M667 {
         new M667().constructArray(3, 2);
     }
 
+
+    // 2020-1-27
+    // 第一直觉想不出来，要用纸演算
+    public int[] constructArray(int n, int k) {
+        if (n <= 0) return new int[] {};
+        int[] res = new int[n];
+        int count = 1;
+        for (int i = 0; i <= k; i += 2) {
+            res[i] = count++;
+        }
+        int j = (k%2==1) ? k : k-1;
+        for (; j>0; j-=2) {
+            res[j] = count++;
+        }
+        while (count<=n) {
+            res[count-1] = count++;
+        }
+
+        return res;
+    }
+
+
+
     // 首先根据k来设置 1 X 2 X 3 X 4 X...
     // 再根据k确定回头的节点，设置X的数字
     // 最后把没有参与排列的数字放到数组尾部
-    public int[] constructArray(int n, int k) {
+    public int[] constructArray_(int n, int k) {
         int[] res = new int[n];
         int count = 1;
         for (int i = 0; i <= k; i += 2) {
