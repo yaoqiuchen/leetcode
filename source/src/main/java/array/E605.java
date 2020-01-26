e975b03188d6d6e9602f8a91a9e2857c76cc26ee
Vincent Yao <qiuchen_yao@126.com>
2020-01-26 08:05:28 +0800

    E605 - 种花问题

--- source/src/main/java/array/E605.java
+++ source/src/main/java/array/E605.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 /**
  605. 种花问题
 
- 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+ 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，
+ 两者都会死去。
 
- 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+ 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。
+ 能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
 
  示例 1:
 
@@ -31,7 +33,26 @@ public class E605 {
     }
 
 
+    // 2020-1-26
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n <= 0) return true;
+        int len = flowerbed.length;
+        for (int i = 0; i < len; i++) {
+            if (flowerbed[i] == 1 ||
+                    (i > 0 && flowerbed[i-1] == 1) ||
+                    (i < len-1 && flowerbed[i+1] == 1)) {
+                continue;
+            }
+            flowerbed[i] = 1;
+            n--;
+            if (n == 0) return true;
+        }
+        return false;
+    }
+
+
+
+    public boolean canPlaceFlowers_(int[] flowerbed, int n) {
         int last = flowerbed.length - 1;
         for (int i = 0; i <= last; i++) {
             if (n == 0) return true;
