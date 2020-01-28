76a226ae14c889c8c49e4bbcfc939bb7d32673b8
Vincent Yao <qiuchen_yao@126.com>
2020-01-28 23:39:57 +0800

    M670 - 最大交换 - 以前的dp做法竟然忘了，不过也做出来了

--- source/src/main/java/array/M670.java
+++ source/src/main/java/array/M670.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 /**
  670. 最大交换
 
@@ -22,13 +24,46 @@ package array;
 public class M670 {
 
     public static void main(String[] args) {
-        new M670().maximumSwap(98368);
+        new M670().maximumSwap(2736);
     }
+
+
+    // 2020-1-28
+    public int maximumSwap(int num) {
+        char input[] = new String(num + "").toCharArray();
+        int len = input.length;
+       ;
+        char[] sorted = Arrays.copyOf(input, len);
+        Arrays.sort(sorted);
+
+        int first = Integer.MAX_VALUE, last = Integer.MAX_VALUE, target = 0;
+        boolean found = false;
+        for (int j = 0; j < len; j++) {
+            if (!found && input[j] != sorted[len-j-1]) {
+                first = j;
+                found = true;
+                target = sorted[len-j-1];
+                continue;
+            }
+            if (found && input[j] == target) {
+                last = j;
+            }
+        }
+
+        if (found) {
+            char tmp = input[first];
+            input[first] = input[last];
+            input[last] = tmp;
+        }
+
+        return Integer.parseInt(new String(input));
+    }
+
 
     // 动态规划的思想，
     // 第一次循环，找到比dp[i]表示比当前数字最大并且处于最远端的数字下标
     // 第二次循环，交换，如果input[i] == input[dp[i]]被认为跳过
-    public int maximumSwap(int num) {
+    public int maximumSwap_(int num) {
         char input[] = new String(num + "").toCharArray();
         int n = input.length - 1;
 
