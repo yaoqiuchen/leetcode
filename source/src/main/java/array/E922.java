cc5c3ed6f723838e66ad49e9225973788c8b7ddb
Vincent Yao <qiuchen_yao@126.com>
2020-02-09 15:06:43 +0800

    E922 - 按奇偶排序数组 II

---
+++ source/src/main/java/array/E922.java
@@ -0,0 +1,51 @@
+package array;
+
+/**
+ *
+ * 922. 按奇偶排序数组 II
+ *
+ 给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+
+ 对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+
+ 你可以返回任何满足上述条件的数组作为答案。
+
+  
+ 示例：
+
+ 输入：[4,2,5,7]
+ 输出：[4,5,2,7]
+ 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+  
+
+ 提示：
+
+ 2 <= A.length <= 20000
+ A.length % 2 == 0
+ 0 <= A[i] <= 1000
+
+ */
+public class E922 {
+
+    public static void main(String[] args) {
+        new E922().sortArrayByParityII(new int[] {-1,-2,-3});
+    }
+
+    // 2020-2-9
+    public int[] sortArrayByParityII(int[] A) {
+        // idx是偶数下标，idx2是奇数下标
+        int idx = 0, idx2 = 1;
+        int[] res = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0 || A[i] % 2 ==0) {
+                res[idx] = A[i];
+                idx += 2;
+            } else {
+                res[idx2] = A[i];
+                idx2 += 2;
+            }
+        }
+        return res;
+    }
+
+}
