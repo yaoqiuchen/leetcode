b0cce97bfe1b39b03db46082d19a3936208b15a9
Vincent Yao <qiuchen_yao@126.com>
2020-02-26 13:38:26 +0800

    E141 - 环形链表

---
+++ source/src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+/**
+ * Created by 半仙.
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
