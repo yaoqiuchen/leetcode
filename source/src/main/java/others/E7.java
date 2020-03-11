01d90bedf6908324516f85344c9c8343131502b2
Vincent Yao <qiuchen_yao@126.com>
2020-03-11 11:15:37 +0800

    E7 - 整数反转

---
+++ source/src/main/java/others/E7.java
@@ -0,0 +1,49 @@
+package others;
+
+import java.math.BigInteger;
+
+/**
+ 7. 整数反转
+ 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+ 示例 1:
+
+ 输入: 123
+ 输出: 321
+ 示例 2:
+
+ 输入: -123
+ 输出: -321
+ 示例 3:
+
+ 输入: 120
+ 输出: 21
+ 注意:
+
+ 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ */
+public class E7 {
+
+    public static void main(String[] args) {
+//        new E176().getPermutation(3, 4);
+    }
+
+    public int reverse(int x) {
+        int rev = 0;
+        while(x != 0){
+            int pop = x % 10;
+            x = x / 10;
+            if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > Integer.MAX_VALUE % 10)){
+                rev = 0;
+                break;
+            }else if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && x < Integer.MIN_VALUE % 10)){
+                rev = 0;
+                break;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+
+}
