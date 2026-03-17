715ed3e1cbe6eb63c6597b9d43fc2dd448b39049
Vincent Yao <qiuchen_yao@126.com>
2026-03-17 10:58:51 +0800

    M12

--- source/src/main/java/string/M12.java
+++ source/src/main/java/string/M12.java
@@ -53,6 +53,26 @@ public class M12 {
 //        new M6().lengthOfLongestSubstring("tmmzuxt");
     }
 
+
+    // 2026/3/17
+    public String intToRoman2(int num) {
+        String[] romaLetters = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] nums = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            int loop = num / val;
+            num = num % val;
+            while (loop-- > 0) {
+                result.append(romaLetters[i]);
+            }
+        }
+
+        return result.toString();
+    }
+
+
     // 3-2-20
     public String intToRoman(int num) {
         String[] romaLetters = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
