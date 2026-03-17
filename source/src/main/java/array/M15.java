58d6fb3a4323f7a3319695cf111f772b2b93d93e
Vincent Yao <qiuchen_yao@126.com>
2026-03-17 14:23:00 +0800

    M15 - sum 3

--- source/src/main/java/array/M15.java
+++ source/src/main/java/array/M15.java
@@ -47,6 +47,46 @@ public class M15 {
 //        new M15().threeSum(new int[] {-2,0,0,2,2});
 //    }
 
+    // 2026/3/17
+    public List<List<Integer>> threeSum3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int l = i+1, r=nums.length-1, target = -nums[i];
+
+            while (l < r) {
+                while (l < r && l > i+1 && nums[l] == nums[l-1]) {
+                    l++;
+                }
+                while (l < r && r < nums.length-1 && nums[r] == nums[r+1]) {
+                    r--;
+                }
+
+                if (l >= r) continue;
+
+                int val = nums[l] + nums[r];
+                if (val == target) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    continue;
+                } else if (val < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return res;
+    }
+
+
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums.length < 3) return new ArrayList<>();
 
