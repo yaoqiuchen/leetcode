911d9144a00a19ad62344c779603c4998b08c12d
Vincent Yao <qiuchen_yao@126.com>
2026-03-24 17:39:17 +0800

    M22 - dfs

--- source/src/main/java/string/M22.java
+++ source/src/main/java/string/M22.java
@@ -27,6 +27,33 @@ public class M22 {
 //        new M6().lengthOfLongestSubstring("tmmzuxt");
     }
 
+    // 2026/3/24
+
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis2(int n) {
+
+        if (n == 0) {
+            return res;
+        }
+
+        dfs("", n, n);
+        return res;
+    }
+
+    public void dfs(String str, int left, int right) {
+        if (left == 0 && right == 0) {
+            res.add(str);
+        }
+        if (left > right) {
+            return;
+        }
+
+        if (left > 0)
+            dfs(str + "(", left-1, right);
+        if (right > 0)
+            dfs(str + ")", left, right-1);
+    }
+
     
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
