aca6006884ac8d79403885cb69e7c6a21e2cf7ee
Vincent Yao <qiuchen_yao@126.com>
2026-04-16 16:41:45 +0800

    M33 - binary search

--- source/src/main/java/array/M33.java
+++ source/src/main/java/array/M33.java
@@ -23,70 +23,40 @@ import java.util.Arrays;
  */
 public class M33 {
 
-    public int search(int[] nums, int target) {
-        int l = 0, h = nums.length - 1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-            if (target == nums[mid]) return mid;
-            if (target == nums[l]) return l;
-            if (target == nums[h]) return h;
-
-            // 左边是排序的
-            if (nums[l] <= nums[mid]) {
-                // 如果范围落在左侧，那么就在左侧找
-                if (target > nums[l] && target < nums[mid]) {
-                    h = mid - 1;
-                } else {
-                    // 否则去右侧找
-                    l = mid + 1;
-                }
-            } else {
-                // 如果左侧是不排序的，那同样的方式在右侧找下
-                if (target > nums[mid] && target < nums[h]) {
-                    l = mid + 1;
-                } else {
-                    // 否则去右侧找
-                    h = mid - 1;
-                }
-            }
-        }
-        return -1;
+    public static void main(String[] args) {
+        new M33().search2(new int[] {4,5,6,7}, 4);
     }
 
-
-
-
-
-
+    // 2026/4/16
     public int search2(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-
-            if (nums[l] == target) return l;
-            if (nums[mid] == target) return mid;
-            if (nums[r] == target) return r;
-
-            // first arr is sorted
-            if (nums[l] <= nums[mid]) {
-                // target in l...mid
-                if (nums[l] < target && nums[mid] > target) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            // first arr is unsorted, so second arr must be sorted
-            else {
-                if (nums[mid+1] <= target && nums[r] > target) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            }
-        }
-        return -1;
+        // 5,6, 1, 2, 3, 4
+        return search2(nums, target, 0, nums.length-1);
     }
+
+    public int search2(int[] nums, int target, int start, int end) {
+        if (start == end) {
+            return nums[start] == target ? start : -1;
+        }
+
+        if (nums[start] < nums[end]) {
+            while (start <= end) {
+                int mid = (start + end)/2;
+                int val = nums[mid];
+                if (target == val) {
+                    return mid;
+                }
+                if (target > val) {
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+            return -1;
+        }
+
+        int mid = (start + end)/2;
+        return Math.max(search2(nums, target, start, mid), search2(nums, target, mid+1, end));
+    }
+
 
 }
